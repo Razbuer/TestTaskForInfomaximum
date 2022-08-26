03c1c4e11ccc662cf4a2c63fab5737d081f0a92b
Razbuer <Rusyaevmax@yandex.ru>
2022-08-26 11:49:34 +0300

    update

--- src/main/java/dev/rusyaev/utils/parsing/XMLParserAddress.java
+++ src/main/java/dev/rusyaev/utils/parsing/XMLParserAddress.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class XMLParserAddress extends AbstractParserAddress implements ParserAddress {
+    @Override
     public Address getAddress(String parseLine) {
         //parseLine = parseLine.replaceAll("\\s*<item","").replaceAll("/>\\s*", "").replaceAll("\\s*=\\s*", "=");
 
